2a366b23134cb4470c312344ed11a722c584399a
Key <34916202+Lumnca@users.noreply.github.com>
2019-11-07 19:34:19 +0800

    68
    68

--- gbts_12_proton/assets/js/courseDatas/courseInfo.js
+++ gbts_12_proton/assets/js/courseDatas/courseInfo.js
@@ -6,299 +6,299 @@
  */
 var course = {
 
-    infoList : [
+    infoList: [
         {
-            icon : 'el-icon-bell',
-            title : '公告',
-            children : [
+            icon: 'el-icon-bell',
+            title: '公告',
+            children: [
                 {
-                   title:  '课程介绍',
-                   href :  'courseNotice.html'
+                    title: '课程介绍',
+                    href: 'courseNotice.html'
                 }
             ],
         },
         {
-            icon : 'el-icon-reading',
-            title : '课件',
-            children : [
+            icon: 'el-icon-reading',
+            title: '课件',
+            children: [
                 {
-                   title:  '课件',
-                   href :  'courseware.html'
+                    title: '课件',
+                    href: 'courseware.html'
                 }
             ],
         },
         {
-            icon : 'el-icon-edit',
-            title : '测试与作业',
-            children : [
+            icon: 'el-icon-edit',
+            title: '测试与作业',
+            children: [
                 {
-                   title:  '作业',
-                   href :  ''
+                    title: '作业',
+                    href: ''
                 },
                 {
-                    title:  '测试',
-                    href :  ''
-                 }
-            ],
-        },
-        {
-            icon : 'el-icon-eleme',
-            title : '考试',
-            children : [
-                {
-                   title:  '考试',
-                   href :  'courseNotice.html'
+                    title: '测试',
+                    href: ''
                 }
             ],
         },
         {
-            icon : 'el-icon-chat-dot-square',
-            title : '讨论区',
-            children : [
+            icon: 'el-icon-eleme',
+            title: '考试',
+            children: [
                 {
-                   title:  '讨论区',
-                   href :  ''
+                    title: '考试',
+                    href: 'courseNotice.html'
                 }
             ],
         },
         {
-            icon : 'el-icon-medal',
-            title : '评价',
-            children : [
+            icon: 'el-icon-chat-dot-square',
+            title: '讨论区',
+            children: [
                 {
-                   title:  '评价',
-                   href :  ''
+                    title: '讨论区',
+                    href: ''
+                }
+            ],
+        },
+        {
+            icon: 'el-icon-medal',
+            title: '评价',
+            children: [
+                {
+                    title: '评价',
+                    href: ''
                 }
             ],
         },
     ],
-    courseOutline : [
+    courseOutline: [
         {
-            name : '第一单元',
-            isComplete : false,
-            children : [
+            name: '第一单元',
+            isComplete: false,
+            children: [
                 {
-                    name : '第一节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第一节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第二节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第二节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第三节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第三节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第四节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第四节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 }
             ]
         },
         {
-            name : '第二单元',
-            isComplete : false,
-            children : [
+            name: '第二单元',
+            isComplete: false,
+            children: [
                 {
-                    name : '第一节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第一节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第二节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第二节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第三节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第三节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第四节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第四节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 }
             ]
         },
         {
-            name : '第三单元',
-            isComplete : false,
-            children : [
+            name: '第三单元',
+            isComplete: false,
+            children: [
                 {
-                    name : '第一节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第一节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第二节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第二节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第三节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第三节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第四节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第四节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 }
             ]
         },
         {
-            name : '第四单元',
-            isComplete : false,
-            children : [
+            name: '第四单元',
+            isComplete: false,
+            children: [
                 {
-                    name : '第一节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第一节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第二节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第二节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第三节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第三节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第四节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第四节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 }
             ]
         },
         {
-            name : '第五单元',
-            isComplete : false,
-            children : [
+            name: '第五单元',
+            isComplete: false,
+            children: [
                 {
-                    name : '第一节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第一节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第二节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第二节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第三节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第三节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 },
                 {
-                    name : '第四节',
-                    isComplete : false,
-                    link : {
-                        video : '',
-                        txt : '',
-                        pdf : '',
-                        word : ''
+                    name: '第四节',
+                    isComplete: false,
+                    link: {
+                        video: '',
+                        txt: '',
+                        pdf: '',
+                        word: ''
                     }
                 }
             ]
@@ -308,39 +308,39 @@ var course = {
 
 
 var userOptions = {
-    order : [
+    order: [
         {
-            title : '查看进度',
-            icon : 'el-icon-collection',
-            href : '#'
+            title: '查看进度',
+            icon: 'el-icon-collection',
+            href: '#'
         },
         {
-            title : '课内消息',
-            icon : 'el-icon-chat-dot-round',
-            href : '#'
+            title: '课内消息',
+            icon: 'el-icon-chat-dot-round',
+            href: '#'
         },
         {
-            title : '退出课堂',
-            icon : 'el-icon-back',
-            href : 'lookUpCourse.html'           
+            title: '退出课堂',
+            icon: 'el-icon-back',
+            href: 'lookUpCourse.html'
         }
     ]
 }
 
 var app = new Vue({
     el: '#app',
-    data : {
-        user : '刘麻子',
-        course : course,
-        userOptions : userOptions,
-        name : window.localStorage.getItem('courseName')
+    data: {
+        user: '刘麻子',
+        course: course,
+        userOptions: userOptions,
+        name: window.localStorage.getItem('courseName')
     },
     methods: {
         handleOpen(key, keyPath) {
-          console.log(key, keyPath);
+            console.log(key, keyPath);
         },
         handleClose(key, keyPath) {
-          console.log(key, keyPath);
+            console.log(key, keyPath);
         }
-      }
+    }
 });
--- gbts_12_proton/assets/js/couse.js
+++ gbts_12_proton/assets/js/couse.js
@@ -1,7 +1,6 @@
 new Vue({
     el : '#app',
     data : {
-       
         course : [
             {
                 name : 'XXX',
@@ -20,7 +19,16 @@ new Vue({
                 src : 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
                 value : '#SO8552',
                 display : false
-            }
+            },
+            {
+              name : 'XXX',
+              numbers : 77,
+              teacher : '张三',
+              level : 3.8,
+              src : 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg',
+              value : '#SO8552',
+              display : false
+          }
         ],
         state : '',
         restaurants: [],
