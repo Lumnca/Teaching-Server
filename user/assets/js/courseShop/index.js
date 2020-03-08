d8772411a4d0d6a179c42bf8957b81c248c2776f
Key <34916202+Lumnca@users.noreply.github.com>
2020-03-08 17:49:20 +0800

    主页完善

--- user/assets/js/courseShop/index.js
+++ user/assets/js/courseShop/index.js
@@ -2,7 +2,15 @@ var app = new Vue({
     el: '#app',
     data: {
         search: '',
-        activeName: '1'
+        activeName: 'style',
+        course : [
+            {
+                type : 'style',
+                name : '教育'
+            },
+            { type : 'shenghuo', name : '生活'},  { type : 'keji', name : '科技'},  { type : 'wenxue', name : '文学'},
+            { type : 'ligong', name : '理工'},  { type : 'xinli', name : '心理'},  { type : 'jishu', name : '技术'},
+        ]
     },
     methods: {
         handleClick(tab, event) {
